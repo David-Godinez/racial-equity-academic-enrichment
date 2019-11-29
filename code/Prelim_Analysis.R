d885bb1a71c2f1479a1190eb9e77abd621f8bbb3
Julia Hoffman <54644313+juliahoffman@users.noreply.github.com>
2019-11-29 14:00:45 -0500

    Update Prelim_Analysis.R

--- code/Prelim_Analysis.R
+++ code/Prelim_Analysis.R
@@ -1,41 +1,80 @@
-
-
 # Preliminary -------------------------------------------------------------
 library(tidyverse)
 library(ggplot2)
-library(devtools)
 library(RCurl)
 library(httr)
 library(dplyr)
 
-
 # Get Data (once repo is public) ----------------------------------------------------------------
 
 School_data <-
+  
   read.csv(
     text = GET(
       "https://github.com/davidgmartinez/final-project/blob/master/data/crdc_tidy_data.csv"
     ),
+    
     header = 1
+    
   )
 
-# Get Data (Download then Upload -- Should be removed once data is --------
+# Get Data (Download then Upload -- Should be removed once data is public)--------
 
-crdc_tidy_data <- read.csv("C:/Users/julia/Downloads/crdc_tidy_data.csv")
+crdc_tidy_data <-
+  read.csv("C:/Users/julia/Downloads/crdc_tidy_data.csv") #would use 'here' if using this long-term
+
 View(crdc_tidy_data)
+
+# Data Prep ---------------------------------------------------------------------
+
+#Adding relevant columns
+
+crdc_tidy_data_2 <-
+  mutate(crdc_tidy_data, prop_white = white / total_enrollment)
+
+crdc_tidy_data_2 <-
+  mutate(
+    crdc_tidy_data_2,
+    enrl_ap_total = enrl_ap_white + enrl_ap_black + enrl_ap_hispanic + enrl_ap_asian + enrl_ap_ind_natv + enrl_ap_hinatv_islndr
+  )
+
+crdc_tidy_data_2 <-
+  mutate(crdc_tidy_data_2, prop_enrl_ap_total = enrl_ap_total / total_enrollment)
+
+#splitting charter and traditional public
+charter_data <- filter(crdc_tidy_data_2, charter == 1)
+trad_public_data <- filter(crdc_tidy_data_2, charter == 0)
 
 
 # EDA ---------------------------------------------------------------------
 
-#Adding relevant columns
-crdc_tidy_data_2 <- mutate(crdc_tidy_data, prop_white = white/total_enrollment)
-crdc_tidy_data_2 <- mutate(crdc_tidy_data_2, enrl_ap_total = enrl_ap_white + enrl_ap_black + enrl_ap_hispanic + enrl_ap_asian + enrl_ap_ind_natv + enrl_ap_hinatv_islndr)
-
-
 
 #General racial distribution of our sample set based on prop_white
-ggplot(data = data_prop_white) + geom_histogram (mapping = aes(x = prop_white), binwidth = .05)
 
+ggplot(data = crdc_tidy_data_2) + geom_histogram (mapping = aes(x = prop_white), binwidth = .05)
 #^mostly non-white (makes sense for urban schools)
 
-ggplot(data = data_prop_white) + geom_point(mapping = aes(x = prop_white, y = enrl_ap_white), na.rm = TRUE)
+
+#White AP enrollment based on proportion white
+ggplot(data = crdc_tidy_data_2) + geom_point(mapping = aes(x = prop_white, y = enrl_ap_white),
+                                             na.rm = TRUE)
+
+#Overall AP enrollment proportion based on white proportion, color charter
+ggplot(data = crdc_tidy_data_2) + geom_point(
+  mapping = aes(x = prop_white, y = prop_enrl_ap_total, color = charter),
+  na.rm = TRUE
+)
+
+#Same as above, but with a regression line
+ggplot(data = crdc_tidy_data_2, aes(x = prop_white, y = prop_enrl_ap_total, color = charter)) + geom_
+point() + geom_smooth(method = 'lm', se = FALSE)
+#!! shows the positive correlation between prop_white and prop_enrl_ap_total
+#(aka overall, more white urban schools have higher proportion enrolled in AP classes)
+
+#CHARTER: Overall AP enrollment proportion based on white proportion
+ggplot(data = charter_data, aes(x = prop_white, y = prop_enrl_ap_total)) + geom_point() + geom_smooth(method = 'lm', se = FALSE)
+#!! negative correlation: more white schools have lower overall AP Enrollment
+
+#PUBLIC: Overall AP enrollment proportion based on white proportion
+ggplot(data = trad_public_data, aes(x = prop_white, y = prop_enrl_ap_total)) + geom_point() + geom_smooth(method = 'lm', se = FALSE)
+#!! positive correlation: more white schools have higher overall AP Enrollment
